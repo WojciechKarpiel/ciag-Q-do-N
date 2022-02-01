fdc52f7a22d5d3d2eae5657f2c769333a69d0daf
Wojciech Karpiel <w.karpiel@avsystem.com>
2022-02-01 20:24:51 +0100

    Posprzątane trochę

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
+# [ℚ] ≅ ℕ
+
 Dowód, że każdy skończony ciąg liczb wymiernych można zakodować jedną liczbą naturalną
 
-Jeszcze nie nadaje się do czytania, choć dowód już jest gotowy
+## Zależności
 
-Do zrobienia
-* posprzątać
-* udowdnić, że to działa w obie strony (tzn istnieje bijekcja N<->[Q])
+W nawiasie podano wersje, które na pewno działają:
+
+* [Coq](https://coq.inria.fr/) (8.14.1)
+* [Mathematical Components](https://math-comp.github.io/): ssreflect, algebra (1.14.0)
+
+
+## Do zrobienia
+Udowdnić, że istnieje bijekcja między skończonymi ciągami liczb wymiernych a liczbami naturalnymi (wzmocnienie obecnego wyniku, który mówi tylko o funkcji różnowartościowej z [Q] w N).
--- ciag-q-do-n.v
+++ ciag-q-do-n.v
@@ -1,11 +1,10 @@
 From mathcomp Require Import all_ssreflect.
-Set Implicit Arguments.
-Unset Strict Implicit.
-Unset Printing Implicit Defensive.
-
 From mathcomp Require Import algebra.rat.
 From mathcomp Require Import ssrint seq.
 
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
 
 (* każdy skończony ciąg liczb wymiernych da się zakodować liczbą naturalną *)
 Definition hipoteza :=
@@ -15,7 +14,7 @@ Definition hipoteza :=
 (* Najpierw potrzeba funkcji parującej.
    Ta jest prosta, ale nie jest bijekcją.
    Za Stefan G. Simpson - "Subsystems of Second Order Arithmetic" (II.2)
-*)
+ *)
 Definition zgniec (para: nat*nat) : nat := let (i, j) := para in ((i+j)^2)+i.
 Check Nat.sqrt.
 Definition odgniec (k: nat): (nat*nat) :=
@@ -48,245 +47,151 @@ Definition odgniec' (n : nat) : nat * nat :=
 (* No ale podmiankę zachowam se na koniec *)
 
 
-Lemma sqrp1 n : n.+1^2 = n^2 + 2*n +1.
+Lemma roznica_miedzy_kwadratami n : n.+1^2 = n^2 + 2*n +1.
 Proof.
   rewrite -addn1 sqrnD -![n^2 + _ + _]addnA [1 ^2 + 2*_]addnC -[1^2]/1 muln1 //.
 Qed.
 
-
-(* 
-PeanoNat.Nat.sqrt_unique:
-  forall a b : nat,
-  ((b * b)%coq_nat <= a)%coq_nat /\ (a < (b.+1 * b.+1)%coq_nat)%coq_nat -> PeanoNat.Nat.sqrt a = b
-*)
+(* Lemat pomocniczy do zgniec_odgniec;
+ w pewnym sensie, to słabsza wersja różnicy między kwadratami *)
 Lemma sqrt_ij i j : Nat.sqrt ((i + j) ^ 2 + i) = (i+j).
 Proof.
   apply: PeanoNat.Nat.sqrt_unique.
   rewrite -[(_ * _)%coq_nat]/((i+j) * (i+j)) -[(_ * _)%coq_nat]/((i+j).+1 * (i+j).+1) !mulnn.
-  split.
-  apply /leP.
-  apply: leq_addr.
-  apply /ltP.
-  rewrite sqrp1.
-  rewrite -(@addnA ((i+j)^2) (2*(i+j)) 1) ltn_add2l.
-  rewrite mul2n -addnn. rewrite -[i in X in X < _]addn0.
-  Check (((i + j) + (i + j)) + 1).
-  Check (addnA 1 2 3).
-  rewrite -[_ + _ + 1 ]addnA -[i+  j + _ ]addnA  ltn_add2l.
-  rewrite addnA addn1.
+  split; [by apply /leP; apply: leq_addr| apply /ltP].
+  rewrite roznica_miedzy_kwadratami  -(@addnA ((i+j)^2) (2*(i+j)) 1) ltn_add2l.
+  rewrite mul2n -addnn -[i in X in X < _]addn0 -[_ + _ + 1 ]addnA -[i+  j + _ ]addnA.
+  rewrite ltn_add2l addnA addn1.
   apply: ltn0Sn.
 Qed.
 
-Lemma aplusbminusa a b : a + b -a = b.
+Lemma a_plus_b_minus_a a b : a + b - a = b.
 Proof.
-  rewrite -[a in X in _+_-X=_]addn0 subnDl subn0 //.
+  by rewrite -[a in X in _+_-X=_]addn0 subnDl subn0.
 Qed.
 
+(* Podstawowy lemat mówiący, że 2 liczby naturalne można zakodować za pomocą jednej
+   cała reszta dowodu bazuje na zgniataniu i odgniataniu par liczb naturalnych *)
 Lemma zgniec_odgniec k : odgniec (zgniec k) = k.
 Proof.
   case: k => i j.
-  rewrite /zgniec /odgniec !sqrt_ij pair_equal_spec !aplusbminusa //.
+  by rewrite /zgniec /odgniec !sqrt_ij pair_equal_spec !a_plus_b_minus_a.
 Qed.
 
-Definition intdnat(z : int) : nat :=
+(* Liczby całkowite można zakodować parą liczb natualnych
+ możliwych kodowań jest dużo, to tutaj nie jest eleganckie,
+ ale łatwo się dowodzi jego odwracalność*)
+Definition int_do_nat(z : int) : nat :=
   match z with
   | ssrint.Posz n => zgniec (n, 0)
   | ssrint.Negz n => zgniec (n, 1)          
   end.
 
-Definition natdint(n : nat) :=
-  let (a,b) := odgniec n in
+Definition nat_do_int(n : nat) :=
+  let (a, b) := odgniec n in
   if b == 0 then Posz a else Negz a.
 
-Lemma intdookola z : natdint (intdnat z) =z.
+Lemma zgniec_odgniec_int z : nat_do_int (int_do_nat z) = z.
 Proof.
-  case : z => n ;  rewrite /natdint /intdnat  zgniec_odgniec //.
+  by case : z => n; rewrite /nat_do_int /int_do_nat  zgniec_odgniec.
 Qed.
 
-Definition ratdnat (r : rat) : nat :=
-  let (a,b) := rat.valq r in  zgniec(intdnat(a), intdnat(b)).
+(* Liczby wymierne można zakodować parą liczb całkowitych *)
+Definition rat_do_nat (r : rat) : nat :=
+  let (a, b) := rat.valq r in zgniec(int_do_nat a, int_do_nat b).
 
 
-Definition natdrat (n : nat): rat .
+(* udziwnienia przy definicji a' i b' mają na celu
+   ułatwienie spełnienia wymagań konstruktora Rat *)
+Definition nat_do_rat (n : nat): rat .
   pose (ab := odgniec n).
-  pose (a := natdint ab.1).
-  pose (b := natdint ab.2).
-  Locate "_ < _".
+  pose (a := nat_do_int ab.1).
+  pose (b := nat_do_int ab.2).
   Open Scope order_scope.
-  pose (b'  :=  (if (Posz 0) < b then b else (Posz 1 : int))).
-  pose (a' := if (coprime `|a| `|b' |) then a else (Posz 1 : int)). 
-  refine (@Rat (a', b') _ ).
-  apply /andP; constructor.
-  rewrite -[(a',b').2]/b' /b'.
-  by case: (ifP).
+  pose (b' := (if (Posz 0) < b then b else (Posz 1))).
   Close Scope order_scope.
-  rewrite -[(_,_).1]/a' -[(_,_).2]/b'.
-  rewrite  /a'. case: ifP => //= _.
-  apply: coprime1n.
+  pose (a' := if (coprime `|a| `|b'|) then a else (Posz 1)). 
+  refine (@Rat (a', b') _ ).
+  apply /andP; split;
+    [by rewrite /b'; case: ifP | rewrite  /a'; case: ifP => //= _; apply: coprime1n].
 Defined.
 
-Lemma ratdookolaq r : natdrat (ratdnat r) == r.
+Lemma zgniec_odgniec_rat r : nat_do_rat (rat_do_nat r) = r.
 Proof.
-  case : r => [[a b]] H.
-  rewrite -[_ == _]/(valq _ == valq _).
-  rewrite -[valq (Rat H)]/(a,b).
-  (* pozbyć się d, bo z nim to lipa *)
-  rewrite /ratdnat .
-  rewrite -[valq (Rat H)]/(a,b).
-
-  
- (* clear d. (* będę tego potrzebował potem, ale dobrze, że nie ma tego na teraz *) *)
-
-  rewrite /natdrat.
-  
-
-  apply /eqP.
-  rewrite [(a,b) in RHS]surjective_pairing. 
-  rewrite  pair_equal_spec.
-  rewrite !zgniec_odgniec !intdookola.
-  split.
-  case: ifP => // X.
-  exfalso.
-  move : H => /andP [x d].
-  rewrite x in X.
-  simpl in d.
-  Search _ (_ = false) _.
-  rewrite X in d .
-  move : d => //.
-
-    move : H => /andP [x d].
-    by     rewrite x.
+  apply /eqP; case : r => [[a b]] H.
+  rewrite -[_ == _]/(valq _ == valq _) -[valq (Rat H)]/(a,b) /rat_do_nat -[valq (Rat H)]/(a,b).
+  (* H nie znajduje się teraz w celu, będzie potrzebne dopiero na końcu  *)
+  rewrite /nat_do_rat; apply /eqP.
+  rewrite [(a,b) in RHS]surjective_pairing pair_equal_spec !zgniec_odgniec !zgniec_odgniec_int.
+  move: H => /andP [zero_ltn_b wzgl_pierwsze]. (* teraz H jest potrzebne *)
+  split; [by case: ifP => //; rewrite zero_ltn_b wzgl_pierwsze | by rewrite zero_ltn_b].
 Qed.
 
+(* Kodowanie ciągu liczb wymiernych o długości n:
+ (n, (a1, a2, a3, a4, ..., a_n, 0),
+ gdzie a_n to zakodowany n-ty wyrac ciągu.
+ Liczba na końcu to wartownik, mogło by go nie być, ale tak było mi łatwiej *)
+Definition seq_do_nat (sq : seq rat) : nat :=
+  zgniec ((length sq), foldr (fun r z => zgniec ((rat_do_nat r), z)) 0 sq).
 
-Lemma ratdookola r : natdrat (ratdnat r) = r.
+(* Funkcja pomocnicza do odkodowywania.
+   Jeśli ciąg jest zakodowany jako (n, (a1, a2, a3,....)),
+   to pierwszy argument funkcji to n, a drugi to (a1, a2 ...) *)
+Fixpoint odgniec_seq' dlugosc wartosci :=
+  match dlugosc with
+  | 0 => [::]
+  | n.+1 => let (a_i, reszta) := odgniec wartosci in (nat_do_rat a_i) :: (odgniec_seq' n reszta)
+  end.
+
+Definition nat_do_seq (n : nat): seq rat :=
+  let (dlugosc, wartosci) := odgniec n in odgniec_seq' dlugosc wartosci.
+
+(* pomocnicze lematy do wykonywania pojedyńczego kroku obliczeń *)
+Lemma fold1krok {A B : Type} (f : A -> B -> B) z q qs : foldr f z (q::qs) = f q (foldr f z qs).
+Proof. done. Qed.
+
+Lemma fold0 {A B : Type} (f : A -> B -> B) z: foldr  f z  [::] = z.
+Proof. done. Qed.
+
+Lemma odgniec1krok dlugosc wartosci :
+  odgniec_seq' dlugosc.+1 wartosci =
+    let (a_i, reszta) := odgniec wartosci in (nat_do_rat a_i) :: (odgniec_seq' dlugosc reszta).
+Proof. done. Qed.
+
+(* Dodatkowo, przy poprawnym rozkodowywaniu wartosci=0, ale to nie istotne *)
+Lemma odgniec0 wartosci : odgniec_seq' 0 wartosci = [::].
+Proof. done. Qed.
+
+
+Lemma seq_do_nat1krok q sq :
+  seq_do_nat (q :: sq) =
+    zgniec (length (q ::sq), zgniec (rat_do_nat q, foldr (fun r z => zgniec ((rat_do_nat r), z)) 0 sq)).
+Proof. done. Qed.
+
+(* Koniec nudnych lematów, teraz zasadnicze mięsko *)
+Lemma nat_do_seq_glowa q qs :
+  nat_do_seq (seq_do_nat (q :: qs)) = q :: (nat_do_seq (seq_do_nat qs)).
 Proof.
-  apply /eqP.
-  apply: ratdookolaq.
+  rewrite seq_do_nat1krok {1}/nat_do_seq zgniec_odgniec.
+  rewrite odgniec1krok. (* czemu "length" się otwiera? nie powinno *)
+  rewrite zgniec_odgniec zgniec_odgniec_rat.
+  apply (f_equal (fun qs => q :: qs)).
+  by rewrite /seq_do_nat /nat_do_seq zgniec_odgniec.
 Qed.
 
-Search "fold".
-Definition seqdonat (sq : seq rat) : nat :=
- zgniec ((length sq),  foldr (fun  r z => zgniec ( (ratdnat r),z)) 0 sq).
-
-
-  (*foldl (fun z r => zgniec (z, (ratdnat r))) (length sq) sq.*)
-  (*foldr (fun  r z => zgniec (z, (ratdnat r))) (length sq) sq.*)
-
-Fixpoint  odgn ln wrt :=
-    match ln with
-    | O => [::]
-    | S n => let (x, r) := odgniec wrt in (natdrat x) :: (odgn  n r)
-    end.
-
-  
-  Definition natdoseq (n : nat): seq rat :=
-  let (len, wartosci) := odgniec n in
-  odgn len wartosci.
-
-
-
-Open Scope rat_scope.
-Definition t1 : rat := 1. (*[rat (2: int) // (3 : int)].*)
-Definition t2 : rat := ratz (2:int). (*[rat (3: int) // (2 : int)].*)
-Definition t3 : rat := [rat (9: int) // (4 : int)].
-Eval compute in (ratdnat t1).
-(* Eval compute in (seqdonat [:: t1]). *)
-Close Scope rat_scope.
-
-
-Lemma foldl1step {A B : Type} (f : A ->B -> A) z q qs : foldl f z (q::qs) = foldl f (f z q) qs.
-Proof. done. Qed.
-
-Lemma fold1step {A B : Type} (f : B -> A-> A) (z:A) (q:B) qs : foldr f z  (q::qs) = f q (foldr  f  z qs) .
-Proof. done. Qed.
-
-Lemma foldl0 {A B : Type} (f : A ->B -> A) z  : foldl f z [::] = z.
-Proof. done. Qed.
-
-Lemma fold0 {A B : Type} (f : A ->B -> B) z  : foldr  f z  [::] = z.
-Proof. done. Qed.
-
-
-Lemma odgn1 ln wrt : odgn ln.+1 wrt = let (x, r) := odgniec wrt in (natdrat x) :: (odgn  ln r).
-Proof. done. Qed.
-
-Lemma odgn0 wrt : odgn 0 wrt = [::].
-Proof. done. Qed.
-
-Lemma seqdookola0  : natdoseq (seqdonat [:: ]) = [:: ]. Proof. done. Qed.
-
-Lemma seqdookola1 a : natdoseq (seqdonat [:: a]) = [:: a].
+Lemma zgniec_odgniec_seq qs : nat_do_seq (seq_do_nat qs) = qs.
 Proof.
-  rewrite /seqdonat.
-  rewrite fold1step fold0. 
-  rewrite /natdoseq.
-  rewrite zgniec_odgniec.
-  rewrite -[length [:: a]]/1.
-  rewrite odgn1 . rewrite zgniec_odgniec // .
-
-  rewrite ratdookola //.
-Qed.
-
-
-Lemma seqdookola2  (a  b: rat):  natdoseq (seqdonat [:: a; b]) = [:: a ;b].
-Proof.
-  rewrite /seqdonat.
-  rewrite fold1step fold1step fold0.
-  rewrite /natdoseq.
-  rewrite zgniec_odgniec.
-  rewrite -[length _]/(0.+1.+1).
-  rewrite odgn1 zgniec_odgniec.
-  rewrite odgn1 zgniec_odgniec odgn0 !ratdookola.
-  done.
-Qed.
-
-(* chyba działa, ale jeszcze się muszę upewnić *) 
-Lemma seqdookola3  (a  b c: rat):  natdoseq (seqdonat [:: a; b;c]) = [:: a ;b;c].
-Proof.
-  rewrite /seqdonat.
-  rewrite !fold1step fold0.
-  rewrite /natdoseq.
-  rewrite zgniec_odgniec.
-  rewrite -[length _]/(3).
-  rewrite odgn1 zgniec_odgniec.
-  rewrite odgn1 zgniec_odgniec.
-  rewrite odgn1 zgniec_odgniec.
-  rewrite odgn0 !ratdookola.
-  done.
-Qed.
-
-Lemma sqdnatdef sq : seqdonat sq =
-                       zgniec ((length sq),  foldr (fun  r z => zgniec ( (ratdnat r),z)) 0 sq).
-Proof. done. Qed.
-
-Lemma sqdnatdef1 a  sq : seqdonat (a ::sq) =
-                       zgniec ((length (a ::sq)), zgniec ( ratdnat a,   foldr (fun  r z => zgniec ( (ratdnat r),z)) 0 (sq))).
-Proof. done. Qed.
-
-
-Lemma eloo a l :natdoseq (seqdonat (a :: l)) = a :: (natdoseq (seqdonat l)).
-Proof.
-  rewrite sqdnatdef1.
-  rewrite {1}/natdoseq zgniec_odgniec.
-  rewrite odgn1. (* czemu to mi się "length" otwiera, to przeszkadza *)
-  rewrite zgniec_odgniec ratdookola.
-  apply (f_equal (fun x => a :: x)).
-  by rewrite /seqdonat /natdoseq zgniec_odgniec.
-Qed.
-
-Lemma seqdookola sq : natdoseq (seqdonat sq) = sq.
-Proof.
-  elim : sq => //q qs H.
+  elim : qs => // q qs H.
   rewrite -[in RHS]H.
-  apply: eloo.
+  apply: nat_do_seq_glowa.
 Qed.
-
-
 
 Lemma twierdzenie: hipoteza.
 Proof.
-  exists natdoseq => qs.
-  exists (seqdonat qs).
-  apply: seqdookola.
+  exists nat_do_seq => qs.
+  exists (seq_do_nat qs).
+  apply: zgniec_odgniec_seq.
 Qed.
+
+(* Teraz udowodnić, że istnieje bijekcja N <-> [Q],
+   taką bijekcją jest zgniec' i odgniec' *)
